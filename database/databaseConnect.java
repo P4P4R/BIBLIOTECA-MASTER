89954b23f70cb8cc04386fca20988e4e86abdfb3
P4P4R <jhunyor.uenp@gmail.com>
2024-12-17 17:21:36 -0300

    first commit

---
+++ database/databaseConnect.java
@@ -0,0 +1,27 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class databaseConnect {
+
+  // configuracoes do bd
+  private static final String URL = "jdbc:postgresql://localhost:5432/biblioteca";
+  private static final String USER = "postgres";
+  private static final String PASS = "3614";
+
+  public static Connection openConnection() throws SQLException {
+    return DriverManager.getConnection(URL, USER, PASS);
+  }
+
+  public static void closeConnection(Connection conn) {
+    if (conn != null) {
+      try {
+        conn.close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
